66805311a2fd94a8817a6060ca728f919886aba1
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2023-01-21 19:57:03 +0100

    heroBanner tailwind styling

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,18 +1,20 @@
-import { storyblokEditable } from "@storyblok/react";
+import {storyblokEditable} from "@storyblok/react";
 import Image from "next/image";
 
-const HeroBanner = ({ blok }) => {
+const HeroBanner = ({blok}) => {
     return (
-        <div {...storyblokEditable(blok)}>
-            <h1>{blok.title}</h1>
-            <p>{blok.text}</p>
-            <p>{blok.button}</p>
+        <div
+            className="bg-red flex justify-center items-center overflow-hidden relative max-h-[480px]"{...storyblokEditable(blok)}>
             {blok.image.filename && blok.image.alt &&
                 <Image width={"1280"} height={"1280"} src={blok.image.filename} alt={blok.image.alt}
-                       className={"h-full w-full object-cover absolute"} loading={"eager"}/>}
+                       className={"h-full w-full object-cover flex-shrink-0"} loading={"eager"}/>}
+
+            <div className={"absolute top-0 bottom-0 left-0 right-0 z-50 flex justify-center items-center text-center text-white flex-col"}>
+                <h1>{blok.title}</h1>
+                <p>{blok.text}</p>
+                <button>{blok.button}</button>
+            </div>
         </div>
-
-
     )
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}"
-],
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "360px",
+      lg: "1280px",
+    },
+    minHeight: {
+      400: "400",
+      560: "560",
+      760: "760",
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
